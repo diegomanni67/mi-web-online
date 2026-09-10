5212022ec326d5d3c426b39a443183c03a2096e6
diegomanni67 <diegomanni67@gmail.com>
2026-09-10 11:33:23 -0300

    Redirect Academy forum to unified community forum

--- app/academy-forum/page.tsx
+++ app/academy-forum/page.tsx
@@ -1,5 +1,5 @@
-import { ForumCategoriesDashboard } from '@/components/forum/ForumCategoriesDashboard'
+import { redirect } from 'next/navigation'
 
 export default function AcademyForumPage() {
-  return <ForumCategoriesDashboard forumType="academy" />
+  redirect('/forum')
 }
